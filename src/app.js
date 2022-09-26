73e557c1c852276a37f7bf0cb5de22d166c46eac
Chaphloziferar <johnhectorlargaespada@gmail.com>
2022-09-25 21:06:31 -0600

    Created login route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import env from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
+
+// Importing Routes
+import authRoutes from './routes/auth.routes';
 
 // Initialize the app
 env.config();
@@ -12,10 +16,14 @@ app.set('port', process.env.PORT || 3000);
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+// Routes Middleware
+app.use('/api/auth', authRoutes);
 
 export default app;
